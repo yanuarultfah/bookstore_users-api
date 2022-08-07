9606822d3701f8980e13ca1e0430048bd9c11357
yanuar.maulana <eragon.shapira10@gmail.com>
2022-08-07 12:15:14 +0700

    refresh

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,1 +1,13 @@
 package users
+
+import (
+	"github.com/yanuarultfah/bookstore_users-api/utils/erorrs"
+)
+
+func Get(userId int64) (*User, *erorrs.RestErr) {
+	return nil, nil
+}
+
+func Save(user User) *erorrs.RestErr {
+	return nil
+}
