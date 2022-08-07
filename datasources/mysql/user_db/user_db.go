1862cb5dffc502973d5543d6e45c04fda6d23639
yanuar.maulana <eragon.shapira10@gmail.com>
2022-08-07 22:04:08 +0700

    refresh

--- datasources/mysql/user_db/user_db.go
+++ datasources/mysql/user_db/user_db.go
@@ -13,13 +13,13 @@ var (
 )
 
 func init() {
-	datasource := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "", "127.0.0.1:3306", "users_db")
+	datasourcename := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "", "127.0.0.1:3306", "users_db")
 	var err error
-	Client, err := sql.Open("mysql", datasource)
+	Client, err := sql.Open("mysql", datasourcename)
 	if err != nil {
 		panic(err)
 	}
-	if err := Client.Ping(); err != nil {
+	if err = Client.Ping(); err != nil {
 		panic(err)
 	}
 	log.Println("database connected........")
