de9a08f4041eaab79495941b8cb41749de26d413
yanuar.maulana <eragon.shapira10@gmail.com>
2022-08-07 21:24:52 +0700

    refresh

--- datasources/mysql/user_db/user_db.go
+++ datasources/mysql/user_db/user_db.go
@@ -1,1 +1,15 @@
 package userdb
+
+import "database/sql"
+
+var (
+	usersDB *sql.DB
+)
+
+func init() {
+	var err error
+	usersDB, err := sql.Open("mysql", "")
+	if err != nil {
+		panic(err)
+	}
+}
